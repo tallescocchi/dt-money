e5f54382c35a0ff68d6749c1f04d83b427e1a144
Talles Cocchi <78833791+tallescocchi@users.noreply.github.com>
2023-03-01 21:37:36 -0300

    First Commit

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,40 @@
+import { Header } from "../../components/Header"
+import { Summary } from "../../components/Summary"
+import { SearchForm } from "./components/SearchForm"
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles"
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+    <TransactionsContainer>
+      <SearchForm />
+      <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>28/02/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 40,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>28/02/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+    </TransactionsContainer>
+    </div>
+  )
+}
