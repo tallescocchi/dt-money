35473f4460a4cf56a89e09fb394839bc2b6d54a3
Talles Cocchi <78833791+tallescocchi@users.noreply.github.com>
2023-03-02 20:09:05 -0300

    Add Contexts

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,15 @@
+import { useContext } from "react"
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
+import { TransactionsContext } from "../../contexts/TransactionsContext"
+import { dateFormatter, priceFormatter } from "../../utils/formatter"
 import { SearchForm } from "./components/SearchForm"
+
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles"
 
 export function Transactions() {
+  const {transactions} = useContext(TransactionsContext)
+
   return (
     <div>
       <Header />
@@ -12,26 +18,21 @@ export function Transactions() {
       <SearchForm />
       <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de site</td>
-              <td>
-                <PriceHighlight variant="income">
-                  R$ 12.000,00
-                </PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>28/02/2023</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburger</td>
-              <td>
-                <PriceHighlight variant="outcome">
-                  - R$ 40,00
-                </PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>28/02/2023</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+                </tr>
+              )
+            })}
           </tbody>
         </TransactionsTable>
     </TransactionsContainer>
